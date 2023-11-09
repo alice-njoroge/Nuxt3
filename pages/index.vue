834bfa42d0b851e2510f78f900b4255fd723715c
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-11-09 16:00:08 +0300

    fetching chart data from a fake API and only displaying the bar chart if the data is available

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,7 @@ import {
   CategoryScale,
   LinearScale,
 } from 'chart.js'
+import {fetch} from "ofetch";
 
 ChartJS.register(
     Title,
@@ -19,30 +20,32 @@ ChartJS.register(
     BarElement,
     CategoryScale,
     LinearScale)
-const chartData = ref<ChartData<"bar">>({
-  labels: [
-    "Spider-Man (2002)",
-    "Spider-Man 2 (2004)",
-    "Spider-Man 3 (2007)",
-    "The Amazing Spider-Man (2012)",
-    "The Amazing Spider-Man 2 (2014)",
-    "Spider-Man: Homecoming (2017)",
-    "Spider-Man: Far From Home (2019)",
-    "Spider-Man: No Way Home (2021)",
-    "Spider-Man: Into the Spider-Verse (2018)",
-    "Spider-Man: Across the Spider-Verse (2023)",
-  ],
-  datasets: [
-    {
-      backgroundColor: ["#c82834", "#244771"],
-      data: [7.3, 7.3, 6.2, 6.9, 6.6, 7.4, 7.5, 8.0, 8.4, 8.9]
-    }
-  ]
-});
+
+//fetching data from the fake API
+type movie = { title: string, rating: number };
+const movies = ref<movie[]>([]);
+fetch("/api.json")
+    .then(async (res) => movies.value = await res.json())
+    .catch(err => console.log(err.message))
+
+const chartData = computed(() : ChartData<"bar"> =>{
+  return {
+    labels: movies.value.map(movie => movie.title),
+    datasets: [
+      {
+        backgroundColor: ["#c82834", "#244771"],
+        data: movies.value.map(movie => movie.rating)
+      }
+    ]
+
+  }
+
+})
+
 </script>
 
 <template>
-  <Bar
+  <Bar v-if="movies.length"
       :data="chartData"
       :options="{
         plugins:{
